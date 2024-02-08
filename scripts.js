76aacbc93b356f6278e2431895d97b33699932b2
Yen Huynh <peterhuynh.contact@gmail.com>
2024-02-08 21:45:50 +0700

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
 var indexHome = ["Ferrari-Home1", "Ferrari-Home2", "Ferrari-Home3", "Ferrari-Home"];
 var homeImg = document.getElementById('home');
 var currentIndex = 0;
+
 // Display home image
 function displayHome() {
     homeImg.style.backgroundImage = "url('images/" + indexHome[currentIndex] + ".jpg')";
